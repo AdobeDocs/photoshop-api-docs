e0d255744f5c8772d586798d30acbbb268547f76
Noah Hoffman <hoffman@adobe.com>
2019-01-04 13:41:14 -0800

    set api version to 0.0.1 to better reflect the prerelease status.  Add valid input range validation to bounds width and height

--- docs/api_project.js
+++ docs/api_project.js
@@ -1,16 +1,1 @@
-define({
-  "name": "PIE in the sky",
-  "version": "1.0.0",
-  "description": "Your interface to PIE ",
-  "title": "PIE in the sky",
-  "url": "https://image-stage.adobe.io/pie",
-  "sampleUrl": false,
-  "defaultVersion": "0.0.0",
-  "apidoc": "0.3.0",
-  "generator": {
-    "name": "apidoc",
-    "time": "2018-11-19T22:33:51.682Z",
-    "url": "http://apidocjs.com",
-    "version": "0.17.7"
-  }
-});
+define({  "name": "PIE in the sky",  "version": "0.0.1",  "description": "Your interface to PIE ",  "title": "PIE in the sky",  "url": "https://image-stage.adobe.io/pie",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2019-01-04T21:36:37.683Z",    "url": "http://apidocjs.com",    "version": "0.17.7"  }});
