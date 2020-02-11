0a51e261ba47294c398924e176fa0c2fbbbe6da1
Noah Hoffman <1139140+noahhoffman@users.noreply.github.com>
2020-02-11 14:54:01 -0800

    set version to 1.0.0

--- docs/api_project.js
+++ docs/api_project.js
@@ -1,10 +1,10 @@
 define({
   "name": "Photoshop API Docs",
-  "version": "0.1.0",
+  "version": "1.0.0",
   "description": "Your interface to Adobe Photoshop and Imaging API's",
   "title": "Photoshop API Docs",
   "sampleUrl": false,
-  "defaultVersion": "0.0.0",
+  "defaultVersion": "1.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
